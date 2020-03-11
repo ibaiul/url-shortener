53ba2aefc3c30c13db08a54bb2b2417eea5b83af
Ibai Usubiaga <ibai.usubiaga@gmail.com>
2020-03-11 20:51:57 +0000

    Removed Error Controller from Swagger

--- src/main/java/eus/ibai/urlshortener/config/SwaggerConfig.java
+++ src/main/java/eus/ibai/urlshortener/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package eus.ibai.urlshortener.config;
 
+import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -8,7 +9,6 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -24,6 +24,7 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
                 .build().apiInfo(apiInfo());
 
     }
